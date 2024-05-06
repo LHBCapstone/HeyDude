f59e6325b951d6c74a0a361a3138ad56c9530920
dldks3467 <dldks3467@naver.com>
2024-05-07 01:01:34 +0900

    build spring

--- src/main/java/com/lhb/lhbackend/controller/MemberController.java
+++ src/main/java/com/lhb/lhbackend/controller/MemberController.java
@@ -8,12 +8,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class MemberController {
-    @PostMapping("/signup")
+    @GetMapping("/signup")
+    @ResponseBody
     public String signup(@RequestBody MemberJoin memberJoin) {
         System.out.println(memberJoin.getEmail());
         System.out.println(memberJoin.getPassword());
         System.out.println(memberJoin.getName());
         System.out.println(memberJoin.getPasswordCheck());
-        return "회원가입 성공";
+
+        return memberJoin.getEmail();
+    }
+    @PostMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "완료!";
+    }
+    @PostMapping("/message")
+    public String receiveMessage(@RequestBody String message) {
+        System.out.println("클라이언트로부터 받은 메시지: " + message);
+        return message; // 받은 메시지를 그대로 응답
     }
 }
