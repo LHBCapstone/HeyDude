a3fd2c379c3bf4b9f9908e5f9b1df5fce11c9f9b
dldks3467 <dldks3467@naver.com>
2024-05-12 19:04:41 +0900

    modify cors

--- src/main/java/com/lhb/lhbackend/controller/MemberController.java
+++ src/main/java/com/lhb/lhbackend/controller/MemberController.java
@@ -4,41 +4,41 @@ import com.lhb.lhbackend.dto.request.MemberJoin;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-public class MemberController {
-
-    @GetMapping("/user/message")
-    public String getMessage() {
-        return "Hello from Spring Boot!";
-    }
-}
-//@RequestMapping("/user")
-//@RequiredArgsConstructor
+//@RestController
 //public class MemberController {
-//    @GetMapping("/signup")
-//    @ResponseBody
-//    public String signup(@RequestBody MemberJoin memberJoin) {
-//        System.out.println(memberJoin.getEmail());
-//        System.out.println(memberJoin.getPassword());
-//        System.out.println(memberJoin.getName());
-//        System.out.println(memberJoin.getPasswordCheck());
 //
-//        return memberJoin.getEmail();
-//    }
-//    @GetMapping("/test")
-//    public String test(@RequestBody String say) {
-//        return "완료!"+say;
-//    }
-//
-//    @GetMapping("/message")
-//    public String receiveMessage(@RequestBody String message) {
-//        System.out.println("클라이언트로부터 받은 메시지: " + message);
-//        return message; // 받은 메시지를 그대로 응답
-//    }
-//    @PostMapping("/checkEmail")
-//    @ResponseBody
-//    public String checkEmail(@RequestParam String email) {
-//        System.out.println(email);
-//        return email;
+//    @GetMapping("/user/message")
+//    public String getMessage() {
+//        return "Hello from Spring Boot!";
 //    }
 //}
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class MemberController {
+    @GetMapping("/signup")
+    @ResponseBody
+    public String signup(@RequestBody MemberJoin memberJoin) {
+        System.out.println(memberJoin.getEmail());
+        System.out.println(memberJoin.getPassword());
+        System.out.println(memberJoin.getName());
+        System.out.println(memberJoin.getPasswordCheck());
+
+        return memberJoin.getEmail();
+    }
+    @GetMapping("/test")
+    public String test(@RequestBody String say) {
+        return "완료!"+say;
+    }
+
+    @GetMapping("/message")
+    public String receiveMessage() {
+        return "hijun"; // 받은 메시지를 그대로 응답
+    }
+    @PostMapping("/checkEmail")
+    @ResponseBody
+    public String checkEmail(@RequestParam String email) {
+        System.out.println(email);
+        return email;
+    }
+}
