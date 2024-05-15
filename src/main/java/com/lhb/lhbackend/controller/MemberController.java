afcb2afe23179eb1cf72e1eaedaa5f1a958aa523
BaekInJu <dldks3467@naver.com>
2024-05-15 23:41:47 +0900

    modify controller

--- src/main/java/com/lhb/lhbackend/controller/MemberController.java
+++ src/main/java/com/lhb/lhbackend/controller/MemberController.java
@@ -56,7 +56,7 @@ public class MemberController {
     @PostMapping("/checkEmail")
     public ResponseEntity<String> checkEmail(@RequestBody String email) {
         if(!memberService.checkEmail(email, memberList)){
-            return ResponseEntity.ok(email);
+            return ResponseEntity.ok(email) ;
         }else{
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("이메일 또는 비밀번호가 올바르지 않습니다.");
         }
